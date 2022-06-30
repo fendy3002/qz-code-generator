45dbb60ca6a1c4f69ca5bf9747fc96045086d6c3
Fendy Heryanto <fendy.fwd@glints.com>
2022-06-30 18:41:59 +0800

    formatting test

--- test/advanced/extension/GroupSupplement.ts
+++ test/advanced/extension/GroupSupplement.ts
@@ -1,33 +1,33 @@
-import changeCase = require('change-case');
 export default async (schema) => {
-    return {
-        name: "GroupSupplement",
-        data: () => {
-            let result = [];
-            for (let field of schema.Model.Fields) {
-                if (field.Gui && field.Gui.Supplement) {
-                    let fieldSupplement = field.Gui.Supplement;
-                    let supplement = result.filter(k => k.ModelName == fieldSupplement.ModelName)[0];
-                    if (!supplement) {
-                        result.push({
-                            ModelName: fieldSupplement.ModelName,
-                            Pair: {
-                                [field.Name]: {
-                                    key: fieldSupplement.Key,
-                                    value: fieldSupplement.Value
-                                }
-                            }
-                        });
-                    }
-                    else {
-                        supplement.Pair[field.Name] = {
-                            key: fieldSupplement.Key,
-                            value: fieldSupplement.Value
-                        };
-                    }
-                }
-            }
-            return result;
+  return {
+    name: 'GroupSupplement',
+    data: () => {
+      let result = [];
+      for (let field of schema.Model.Fields) {
+        if (field.Gui && field.Gui.Supplement) {
+          let fieldSupplement = field.Gui.Supplement;
+          let supplement = result.filter(
+            (k) => k.ModelName == fieldSupplement.ModelName,
+          )[0];
+          if (!supplement) {
+            result.push({
+              ModelName: fieldSupplement.ModelName,
+              Pair: {
+                [field.Name]: {
+                  key: fieldSupplement.Key,
+                  value: fieldSupplement.Value,
+                },
+              },
+            });
+          } else {
+            supplement.Pair[field.Name] = {
+              key: fieldSupplement.Key,
+              value: fieldSupplement.Value,
+            };
+          }
         }
-    }
+      }
+      return result;
+    },
+  };
 };
